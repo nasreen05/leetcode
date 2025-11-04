9b32aff7d7199a0dba503d2d66962efa8c708605
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-11-04 09:51:11 +0530

    Time: 5 ms (36.01%) | Memory: 46.8 MB (5.81%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);  // base index for valid substring calculation
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
+        }
+        return maxLen;
+    }
+}
