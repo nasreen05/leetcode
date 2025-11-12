c6ea7bf8195612f59f364055577e2a7c45bdf6e1
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-11-12 12:42:10 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (7.13%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+        
+        long exp = n; // Use long to handle n = -2^31
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+        
+        double result = 1.0;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            exp /= 2;
+        }
+        
+        return result;
+    }
+}
