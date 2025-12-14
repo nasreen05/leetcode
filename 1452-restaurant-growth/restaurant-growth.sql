329c0731becbc2107df9a88ec3cafd2b036af5ef
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-12-14 10:43:15 +0530

    Time: 302 ms (97.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+SELECT
+    a.visited_on,
+    SUM(b.amount) AS amount,
+    ROUND(SUM(b.amount) / 7, 2) AS average_amount
+FROM
+    (SELECT visited_on, SUM(amount) AS amount
+     FROM Customer
+     GROUP BY visited_on) a
+JOIN
+    (SELECT visited_on, SUM(amount) AS amount
+     FROM Customer
+     GROUP BY visited_on) b
+ON
+    b.visited_on BETWEEN DATE_SUB(a.visited_on, INTERVAL 6 DAY)
+                     AND a.visited_on
+GROUP BY
+    a.visited_on
+HAVING
+    COUNT(b.visited_on) = 7
+ORDER BY
+    a.visited_on;
